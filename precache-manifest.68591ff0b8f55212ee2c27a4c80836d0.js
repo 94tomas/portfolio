cb30ff7b95229f17a8496bc8833caba544a376be
94tomas <nos18de@gmail.com>
2021-01-19 23:01:16 -0400

    deploy

---
+++ precache-manifest.68591ff0b8f55212ee2c27a4c80836d0.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976ad70d93ebdb3e7d9f",
+    "url": "/portfolio/css/app.68fb0c2d.css"
+  },
+  {
+    "revision": "fe766434baa8d72f598c",
+    "url": "/portfolio/css/chunk-vendors.67042d3d.css"
+  },
+  {
+    "revision": "6e52668b49ed34acfbad785ef044bdf0",
+    "url": "/portfolio/img/background_custom.6e52668b.png"
+  },
+  {
+    "revision": "1a89d2ac2972a721a209ab236b7db8ad",
+    "url": "/portfolio/img/caminovalley.1a89d2ac.png"
+  },
+  {
+    "revision": "c964b38f365d6f0dbb1725444c7463e3",
+    "url": "/portfolio/img/caminovalley2.c964b38f.png"
+  },
+  {
+    "revision": "46ec192bd87bbb957f97b1e057730dc7",
+    "url": "/portfolio/img/caminovalley_mobile.46ec192b.png"
+  },
+  {
+    "revision": "3031b53918840e9e7ed8f64422b16fdc",
+    "url": "/portfolio/img/certificate.3031b539.png"
+  },
+  {
+    "revision": "97a79f74dfbe52d9400d84c81eb563f0",
+    "url": "/portfolio/img/code.97a79f74.png"
+  },
+  {
+    "revision": "97a79f74dfbe52d9400d84c81eb563f0",
+    "url": "/portfolio/img/code.png"
+  },
+  {
+    "revision": "c1841c59a6c6c727150d6385f95eb0d3",
+    "url": "/portfolio/img/code2.c1841c59.png"
+  },
+  {
+    "revision": "e87b5f4c4b5032d5d0ac59dddfe2297d",
+    "url": "/portfolio/img/contact.e87b5f4c.png"
+  },
+  {
+    "revision": "c99ba0d1414e3f504d887d5a67a24460",
+    "url": "/portfolio/img/cool-background.c99ba0d1.png"
+  },
+  {
+    "revision": "1552241c0577561b2ca4f9c23aac71a2",
+    "url": "/portfolio/img/cv.1552241c.png"
+  },
+  {
+    "revision": "644e84372f11d219d15debfb00d2209e",
+    "url": "/portfolio/img/cv2.644e8437.png"
+  },
+  {
+    "revision": "b35955d1a557a370c05b5b65ad90eb89",
+    "url": "/portfolio/img/fivo.b35955d1.png"
+  },
+  {
+    "revision": "68930992bc0c5d64b6437c3ef4d74c42",
+    "url": "/portfolio/img/fivo_mobile.68930992.png"
+  },
+  {
+    "revision": "26908716782271887bcc88ca7e0bd027",
+    "url": "/portfolio/img/home.26908716.png"
+  },
+  {
+    "revision": "2e1d5c175c8f36ec958d40ecc1890b29",
+    "url": "/portfolio/img/home.2e1d5c17.png"
+  },
+  {
+    "revision": "8ee064ce9aba13f5a4dce9374d941364",
+    "url": "/portfolio/img/home.8ee064ce.png"
+  },
+  {
+    "revision": "ca4591e31c9ae7fd37e6306634cb0819",
+    "url": "/portfolio/img/home.ca4591e3.png"
+  },
+  {
+    "revision": "e76e79295bcb5424edcf201af8004583",
+    "url": "/portfolio/img/home.e76e7929.png"
+  },
+  {
+    "revision": "6c8ec4bb9d9b5c18f1d345b9a15da5d4",
+    "url": "/portfolio/img/homepage-nexos.6c8ec4bb.png"
+  },
+  {
+    "revision": "36f7d69f7fbdccc14927ef05e89d90ce",
+    "url": "/portfolio/img/image1.36f7d69f.png"
+  },
+  {
+    "revision": "4791284bfd010ddc4e9e0bc7837a8f69",
+    "url": "/portfolio/img/image1.4791284b.png"
+  },
+  {
+    "revision": "8380b212a198ee366415ff0b475deda7",
+    "url": "/portfolio/img/image1.8380b212.png"
+  },
+  {
+    "revision": "a3e2b29dc5182ff7bdc804e229cfa8fb",
+    "url": "/portfolio/img/image1.a3e2b29d.png"
+  },
+  {
+    "revision": "0a0a4cd3671976260f196bc9c9e75286",
+    "url": "/portfolio/img/image2.0a0a4cd3.png"
+  },
+  {
+    "revision": "1a3850868750c5d91c6a3063fa1c29c9",
+    "url": "/portfolio/img/image2.1a385086.png"
+  },
+  {
+    "revision": "454ec5aac4663cd1ff5fdbdd4776f14f",
+    "url": "/portfolio/img/image2.454ec5aa.png"
+  },
+  {
+    "revision": "9880d27921b073d5db900d23208c5d19",
+    "url": "/portfolio/img/image2.9880d279.png"
+  },
+  {
+    "revision": "b62df69ca770ac470f6e6b11ead2c2c0",
+    "url": "/portfolio/img/laptop-nexos.b62df69c.png"
+  },
+  {
+    "revision": "38a515fc62ea4cdaaadddd3f1c2fb33b",
+    "url": "/portfolio/img/panel.38a515fc.png"
+  },
+  {
+    "revision": "07d98ba60cec95cfe12c095def1c71f4",
+    "url": "/portfolio/img/vikinger.07d98ba6.png"
+  },
+  {
+    "revision": "b43c518253db69fa3e975a728f43e184",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56035a717d27261ed700",
+    "url": "/portfolio/js/about.e97fb03c.js"
+  },
+  {
+    "revision": "976ad70d93ebdb3e7d9f",
+    "url": "/portfolio/js/app.cc74eda9.js"
+  },
+  {
+    "revision": "fe766434baa8d72f598c",
+    "url": "/portfolio/js/chunk-vendors.0b55ea27.js"
+  },
+  {
+    "revision": "3d1011ddebc26a285f7181a71b44204c",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
